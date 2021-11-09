64e705f43991efb4e9f27af56241159740151753
Spencer Clarke <sdc2@njit.edu>
2021-11-09 12:46:03 -0500

    Finally fixed login problem (hopefully

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -55,7 +55,7 @@ require(__DIR__."/../../partials/nav.php");?>
      //sanitize
      $email = filter_var($email, FILTER_SANITIZE_EMAIL);
      //validate
-     if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($username)){
+     if(!filter_var($email, FILTER_VALIDATE_EMAIL) && empty($username)){
          flash("Invalid email", "warning");
          $hasErrors = true;
      }
