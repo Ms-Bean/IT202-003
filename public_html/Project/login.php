c1f12fbbfa7df953b91077cc3bbffd018b0c9221
Spencer Clarke <sdc2@njit.edu>
2021-11-09 12:23:18 -0500

    e

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -18,6 +18,8 @@ require(__DIR__."/../../partials/nav.php");?>
     <div>
         <label for="username">Username</label>
         <input type="text" name="username"/>
+    </div>
+    <div>
         <label for="email">Email</label>
         <input type="email" name="email" required maxlength="30"/>
     </div>
