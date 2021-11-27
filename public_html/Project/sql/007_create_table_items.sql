155ab3b87bfb6a4b7742298503287044ca58c406
Spencer Clarke <sdc2@njit.edu>
2021-11-27 18:52:47 -0500

    Fixed table again

--- public_html/Project/sql/007_create_table_items.sql
+++ public_html/Project/sql/007_create_table_items.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS Products(
     id int AUTO_INCREMENT PRIMARY  KEY,
     name varchar(30) UNIQUE,
-    category varchar(30),
     description text,
+    category varchar(30),
     stock int DEFAULT  0,
-    cost int DEFAULT  99999,
-    image text,
-    visibility boolean DEFAULT true,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+    unit_price int DEFAULT  99999,
+    visibility varchar(30)
 )
