6697b156145bbdaf9275837d5336866bbc77aeb8
Spencer Clarke <sdc2@njit.edu>
2021-11-27 18:41:00 -0500

    Added shop

--- public_html/Project/sql/007_create_table_items.sql
+++ public_html/Project/sql/007_create_table_items.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS Products(
     id int AUTO_INCREMENT PRIMARY  KEY,
     name varchar(30) UNIQUE,
+    category varchar(30),
     description text,
     stock int DEFAULT  0,
     cost int DEFAULT  99999,
