55fcd4a87039f8b90d991bf62d3c884948c873f4
Spencer Clarke <sdc2@njit.edu>
2021-11-28 20:55:15 -0500

    Fixed formatting issue

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -31,15 +31,15 @@ try {
 <div class="container-fluid">
     <h1>Cart</h1>
     <?php
-        foreach($results as $index => $record)
-            foreach($record as $column => $value)
+        foreach($results as $index => $record) : 
+            foreach($record as $column => $value) :
     ?>
     <div class="cart_item">
         <?php
             if($column === 'product_id'){
                 $stmt = $db->prepare("SELECT name from Products WHERE id = :product_id");
                 try {
-                    $stmt->execute([":product_id" => $column]);
+                    $stmt->execute([":product_id" => $product_column]);
                     $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     if ($r) {
                         $results_products = $r;
@@ -57,6 +57,7 @@ try {
             }
         ?>
     </div>
+    <?php endforeach; endforeach;?>
 </div>
 <?php
 require_once(__DIR__ . "/../../partials/flash.php");
