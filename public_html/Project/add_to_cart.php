b4bebc5f320a9c04817f8afbaac8e5b36d846806
Spencer Clarke <sdc2@njit.edu>
2021-11-28 19:11:23 -0500

    Tried fixing problem by removing item name

--- public_html/Project/add_to_cart.php
+++ public_html/Project/add_to_cart.php
@@ -50,7 +50,7 @@ if (isset($_POST["submit"])) {
 
 </style>
 <div class="container-fluid">
-    <h1>Add <?php echo($name)?> To Cart</h1>
+    <h1>Add To Cart</h1>
     <form method="POST">
         <div>
             <label for="desired_quantity">Quantity</label>
