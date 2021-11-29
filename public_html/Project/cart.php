e90d8d4b4116ad68422628c3445e47723b7ca496
Spencer Clarke <sdc2@njit.edu>
2021-11-28 19:42:52 -0500

    Added initial cart displayer

---
+++ public_html/Project/cart.php
@@ -0,0 +1,37 @@
+<?php
+require(__DIR__ . "/../../../partials/nav.php");
+
+if (!has_role("Admin")) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: $BASE_PATH" . "login.php"));
+}
+
+$results = [];
+if (isset($_POST["itemName"])) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT product_id, cost from CartItems WHERE id = :id");
+    try {
+        $stmt->execute([":id" => $_SESSION['user']['id']]);
+        $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($r) {
+            $results = $r;
+        }
+    } catch (PDOException $e) {
+        flash("<pre>" . var_export($e, true) . "</pre>");
+    }
+}
+?>
+<style>
+</style>
+<div class="container-fluid">
+    <?php
+        foreach($results as $index => $record){
+            foreach($record as $column => $value){
+                echo($column . ": " . $value . "<br>");
+            }
+        }
+    ?>
+</div>
+<?php
+require_once(__DIR__ . "/../../../partials/flash.php");
+?>
