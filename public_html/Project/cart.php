6a55db45dd05afc6bc2503fbce195f614ae634cd
Spencer Clarke <sdc2@njit.edu>
2021-11-29 09:02:16 -0500

    Put things back where they belong

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -13,23 +13,7 @@ try {
 } catch (PDOException $e) {
     flash("<pre>" . var_export($e, true) . "</pre>");
 }
-foreach($ids as $current_id){
-    flash($id);
-    if(isset($_POST["submit" . $current_id])){
-        $quantity_to_insert = se($_POST, "quantity" . $current_id, "", false);
-        $stmt = $db->prepare("UPDATE CartItems SET desired_quantity= :desired_quantity WHERE id= :id");
-        try {
-            $stmt->execute([":desired_quantity" => $quantity_to_insert, ":id" => $current_id]);
-            flash("Updated value");
-        } catch (Exception $e) {
-            flash("<pre>" . var_export($e, true) . "</pre>");
-        }
-    }
-}
 
-if(isset($_POST["submit2"])){
-    flash("Button for 2 clicked");
-}
 ?>
 <style>
     .cart_item{
@@ -92,7 +76,23 @@ if(isset($_POST["submit2"])){
         }
     ?>
     <?php
+        foreach($ids as $current_id){
+            flash($id);
+            if(isset($_POST["submit" . $current_id])){
+                $quantity_to_insert = se($_POST, "quantity" . $current_id, "", false);
+                $stmt = $db->prepare("UPDATE CartItems SET desired_quantity= :desired_quantity WHERE id= :id");
+                try {
+                    $stmt->execute([":desired_quantity" => $quantity_to_insert, ":id" => $current_id]);
+                    flash("Updated value");
+                } catch (Exception $e) {
+                    flash("<pre>" . var_export($e, true) . "</pre>");
+                }
+            }
+        }
         
+        if(isset($_POST["submit2"])){
+            flash("Button for 2 clicked");
+        }
     ?>
 </div>
 <?php
