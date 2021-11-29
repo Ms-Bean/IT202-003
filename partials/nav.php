5fa3b934a43cb8181800ac9af0c067b14a809f04
Spencer Clarke <sdc2@njit.edu>
2021-11-29 11:03:45 -0500

    Fixed navbar links

--- partials/nav.php
+++ partials/nav.php
@@ -40,24 +40,24 @@ require_once(__DIR__ . "/../lib/functions.php");
 <nav class="nav_bar">
 <ul>
         <?php if (is_logged_in()) : ?>
-            <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
-            <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
-            <li><a href="<?php echo get_url('shop.php'); ?>">Shop</a></li>
-            <li><a href="<?php echo get_url('cart.php'); ?>">Cart</a></li>
+            <li><a href="<?php echo __DIR__ . "/home.php"; ?>">Home</a></li>
+            <li><a href="<?php echo __DIR__ . "/profile.php"; ?>">Profile</a></li>
+            <li><a href="<?php echo __DIR__ . "/shop.php"; ?>">Shop</a></li>
+            <li><a href="<?php echo __DIR__ . "/cart.php"; ?>">Cart</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
-            <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
-            <li><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
+            <li><a href="<?php echo __DIR__ . "/login.php"; ?>">Login</a></li>
+            <li><a href="<?php echo __DIR__ . "/register.php"; ?>">Register</a></li>
         <?php endif; ?>
         <?php if (has_role("Admin")) : ?>
-            <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
-            <li><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
-            <li><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
-            <li><a href="<?php echo get_url('admin/add_item.php'); ?>">Add Items</a></li>
-            <li><a href="<?php echo get_url('admin/list_items.php'); ?>">List Items</a></li>
+            <li><a href="<?php echo __DIR__ . "/admin/create_role.php"; ?>">Create Role</a></li>
+            <li><a href="<?php echo __DIR__ . "/admin/list_roles.php"; ?>">List Roles</a></li>
+            <li><a href="<?php echo __DIR__ . "/admin/assign_roles.php"; ?>">Assign Roles</a></li>
+            <li><a href="<?php echo __DIR__ . "/admin/add_item.php"; ?>">Add Items</a></li>
+            <li><a href="<?php echo __DIR__ . "/admin/list_items.php"; ?>">List Items</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
-            <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
+            <li><a href="<?php echo __DIR__ . "/logout.php"; ?>">Logout</a></li>
         <?php endif; ?>
     </ul>
 </nav>
