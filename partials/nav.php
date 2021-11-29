37f68b3913ec70b526b35eab220282f7276f1f80
Spencer Clarke <sdc2@njit.edu>
2021-11-29 11:07:48 -0500

    Fixed navbar links

--- partials/nav.php
+++ partials/nav.php
@@ -40,24 +40,24 @@ require_once(__DIR__ . "/../lib/functions.php");
 <nav class="nav_bar">
 <ul>
         <?php if (is_logged_in()) : ?>
-            <li><a href="<?php echo __DIR__ . "/../Project/home.php"; ?>">Home</a></li>
-            <li><a href="<?php echo __DIR__ . "/../Project/profile.php"; ?>">Profile</a></li>
-            <li><a href="<?php echo __DIR__ . "/../Project/shop.php"; ?>">Shop</a></li>
-            <li><a href="<?php echo __DIR__ . "/../Project/cart.php"; ?>">Cart</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/home.php"; ?>">Home</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/profile.php"; ?>">Profile</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/shop.php"; ?>">Shop</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/cart.php"; ?>">Cart</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
-            <li><a href="<?php echo __DIR__ . "/../Project/login.php"; ?>">Login</a></li>
-            <li><a href="<?php echo __DIR__ . "/../Project/register.php"; ?>">Register</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/login.php"; ?>">Login</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/register.php"; ?>">Register</a></li>
         <?php endif; ?>
         <?php if (has_role("Admin")) : ?>
-            <li><a href="<?php echo __DIR__ . "/../admin/create_role.php"; ?>">Create Role</a></li>
-            <li><a href="<?php echo __DIR__ . "/../Project/admin/list_roles.php"; ?>">List Roles</a></li>
-            <li><a href="<?php echo __DIR__ . "/../Project/admin/assign_roles.php"; ?>">Assign Roles</a></li>
-            <li><a href="<?php echo __DIR__ . "/../Project/admin/add_item.php"; ?>">Add Items</a></li>
-            <li><a href="<?php echo __DIR__ . "/../Project/admin/list_items.php"; ?>">List Items</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../admin/create_role.php"; ?>">Create Role</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/admin/list_roles.php"; ?>">List Roles</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/admin/assign_roles.php"; ?>">Assign Roles</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/admin/add_item.php"; ?>">Add Items</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/admin/list_items.php"; ?>">List Items</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
-            <li><a href="<?php echo __DIR__ . "/../Project/logout.php"; ?>">Logout</a></li>
+            <li><a href="<?php echo __DIR__ . "/../../Project/logout.php"; ?>">Logout</a></li>
         <?php endif; ?>
     </ul>
 </nav>
