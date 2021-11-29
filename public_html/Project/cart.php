8d409d164b314f8a674a81f1c1acff28880d44e1
Spencer Clarke <sdc2@njit.edu>
2021-11-29 09:15:22 -0500

    Delete cart item if quantity = 0

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -80,6 +80,10 @@ try {
             flash($id);
             if(isset($_POST["submit" . $current_id])){
                 $quantity_to_insert = se($_POST, "quantity" . $current_id, "", false);
+                $sqlstr = "UPDATE CartItems SET desired_quantity= :desired_quantity WHERE id= :id";
+                if($quantity_to_insert == 0){
+                    $sqlstr = "DELETE FROM CartItems WHERE id= :id";
+                }
                 $stmt = $db->prepare("UPDATE CartItems SET desired_quantity= :desired_quantity WHERE id= :id");
                 try {
                     $stmt->execute([":desired_quantity" => $quantity_to_insert, ":id" => $current_id]);
@@ -88,10 +92,6 @@ try {
                     flash("<pre>" . var_export($e, true) . "</pre>");
                 }
             }
-        }
-        
-        if(isset($_POST["submit2"])){
-            flash("Button for 2 clicked");
         }
     ?>
 </div>
