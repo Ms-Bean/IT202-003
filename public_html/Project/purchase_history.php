83bf7b1da7beac01c34ee573a19b3b71db620c01
Spencer Clarke <sdc2@njit.edu>
2021-12-18 10:27:49 -0500

    Added closing tag

--- public_html/Project/purchase_history.php
+++ public_html/Project/purchase_history.php
@@ -117,20 +117,8 @@ foreach($orders_results as $index => $record){
 if(has_role("Owner")){
     echo("<h1>Total: " . $total . "</h1>");
 }
-echo('<a href="purchase_history.php?page=1'); 
-if($isset($_POST["sorter"])){
-    echo("&sorter=" . $_POST["sorter"]);
-}
-if($_POST["by_category"] !== ''){
-    echo("&by_category=" . $_POST["by_category"]);
-}
-if($_POST["start_date_range"] !== ''){
-    echo("&start_date_range=" . $_POST["start_date_range"]);
-}
-if($_POST["end_date_range"] !== ''){
-    echo("&end_date_range=" . $_POST["end_date_range"]);
-}
-echo('">Page 2</a>');
+echo('<a href="purchase_history.php?page=1">aaa</a>'); 
+
 ?>
 <?php require(__DIR__ . "/../../partials/flash.php");
 ?>
