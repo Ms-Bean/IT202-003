f9fdecb2e2d66cd4163823ae8ed82d788e00cd7f
Spencer Clarke <sdc2@njit.edu>
2021-12-18 23:59:02 -0500

    bruh

--- public_html/Project/purchase_history.php
+++ public_html/Project/purchase_history.php
@@ -76,7 +76,7 @@ if($by_total){
 $sql_str = $sql_str . " LIMIT "  . $current_page*$PER_PAGE . "," . $PER_PAGE . " ";
 $count_str = "SELECT COUNT(*) FROM " . explode('LIMIT', explode('FROM', $sql_str)[1])[0]; //Circumcise the sql string in order to obtain count
 echo($sql_str);
-echo("Count: " . $count_str[0]);
+echo("Count: " . $count_str);
 $stmt = $db->prepare($sql_str);
 try {
     $stmt->execute([":user_id" => $_SESSION["user"]["id"]]);
@@ -100,7 +100,7 @@ try {
 ?>
 <h1>Order History</h1>
 <?php
-echo($count_results);
+echo($count_results[0]);
 foreach($orders_results as $index => $record){
     echo("<div class='order_info'>");
     echo("<br>Order " . $record["id"] . " placed on " . $record["created"]);
