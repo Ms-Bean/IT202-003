83d015cf84cc9ecbca6b61fff7da5c42be3501c0
Spencer Clarke <sdc2@njit.edu>
2021-12-21 06:17:31 -0500

    Turned list_items into a modified form of the shop page

--- public_html/Project/admin/list_items.php
+++ public_html/Project/admin/list_items.php
@@ -1,80 +1,131 @@
 <?php
-require(__DIR__ . "/../../../partials/nav.php");
-
-if (!has_role("Admin")) {
-    flash("You don't have permission to view this page", "warning");
-    die(header("Location: $BASE_PATH" . "login.php"));
-}
-
+require(__DIR__ . "../../../partials/nav.php");
+$PER_PAGE = 10;
 $results = [];
-if (isset($_POST["itemName"])) {
-    $db = getDB();
-    $stmt = $db->prepare("SELECT id, name, description, category, stock, cost from Products WHERE name like :name LIMIT 50");
-    try {
-        $stmt->execute([":name" => "%" . $_POST["itemName"] . "%"]);
-        $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        if ($r) {
-            $results = $r;
-        }
-    } catch (PDOException $e) {
-        flash("<pre>" . var_export($e, true) . "</pre>");
+$itemCategory = "";
+$itemName = "";
+$sortPrice = "";
+$sortRating = "";
+$outOfStock = "";
+$current_page = 0;
+if(isset($_GET["current_page"])){
+    $current_page = $_GET["current_page"];
+}
+if(isset($_GET["itemCategory"])){
+    $itemCategory = $_GET["itemCategory"];
+}
+if(isset($_GET["outOfStock"])){
+    $outOfStock = "true";
+}
+if(isset($_GET["itemName"])){
+    $itemName = $_GET["itemName"];
+}
+if(isset($_GET["sortPrice"])){
+    $sortPrice = "true";
+}
+if(isset($_POST["submit"])){
+    if(isset($_POST["itemName"])){
+        $itemName = $_POST["itemName"];
+    }
+    if(isset($_POST["itemCategory"])){
+        $itemCategory = $_POST["itemCategory"];
+    }
+    if(isset($_POST["sortPrice"])){
+        $sortPrice = true;
+    }
+    if(isset($_POST["outOfStock"])){
+        $outOfStock = true;
+    }
+    else{
+        $sortPrice = false;
     }
 }
-?>
-<style>
-    table, tr, th, td{
-        border: 1px solid black;
-    }
-    td{
-        width: 14%;
-    }
-    .image{
-        border: 1px solid black;
-        height: 100px;
-        width: 100px;
-    }
-    td{
+$params = [];
+$sqlstr = "SELECT * FROM Products WHERE 1=1 AND stock > 0";
+if(!empty($itemCategory)){
+    $sqlstr = $sqlstr . " AND category = :itemCategory";
+    $params[":itemCategory"] = $itemCategory;
+}
+if(!empty($itemName)){
+    $sqlstr = $sqlstr . " AND name like :itemName";
+    $params[":itemName"] = "%" . $itemName . "%";
+}
+if(!empty($outOfStock)){
+    $sqlstr = $sqlstr . " AND stock = 0";
+}
+if(!empty($sortPrice)){
+    $sqlstr = $sqlstr . " ORDER BY cost";
+}
 
+$sqlstr .= " LIMIT " . $current_page * $PER_PAGE . ","  . $PER_PAGE;
+$count_str = "SELECT COUNT(*) FROM " . explode('LIMIT', explode('FROM', $sqlstr)[1])[0]; //Circumcise the sql string in order to obtain count
+$db = getDB();
+$stmt = $db->prepare($sqlstr);
+try {
+    $stmt->execute($params);
+    $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($r) {
+        $results = $r;
     }
-</style>
+} catch (PDOException $e) {
+    flash("<pre>" . var_export($e, true) . "</pre>");
+}
+$stmt = $db->prepare($count_str);
+try {
+    $stmt->execute($params);
+    $r = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($r) {
+        $count_results = $r;
+    }
+} catch (PDOException $e) {
+    flash("<pre>" . var_export($e, true) . "</pre>");
+}
+
+?>
 <div class="container-fluid">
-    <h1>List Items</h1>
-    <form method="POST" class="row row-cols-lg-auto g-3 align-items-center">
-        <div class="input-group mb-3">
-            <input class="form-control" type="search" name="itemName" placeholder="Item Filter" />
-            <input class="btn btn-primary" type="submit" value="Search" />
+    <h1>Shop</h1>
+    <form method="POST">
+        <div>
+            <input type="search" name="itemName" placeholder="Item Filter" /><br>
+            <input type="search" name="itemCategory" placeholder="Category Filter" /><br>
+            <input type="checkbox" name="sortPrice" value="Sort by price"/> Sort by price<br>
+            <input type="checkbox" name="outOfStock" value="Out of stock"/> Search items out of stock<br>
+            <input type="submit" name="submit" value="Search"/>
         </div>
     </form>
-    <?php if (count($results) == 0) : ?>
-        <p>No results to show</p>
-    <?php else : ?>
-        <table class="table">
-            <?php foreach ($results as $index => $record) : ?>
-                <?php if ($index == 0) : ?>
-                    <thead>
-                        <?php foreach ($record as $column => $value) : ?>
-                            <th><?php se($column); ?></th>
-                        <?php endforeach; ?>
-                        <th>Actions</th>
-                    </thead>
-                <?php endif; ?>
-                <tr>
-                    <?php foreach ($record as $column => $value) : ?>
-                        <td><?php 
-                            $v = se($value, null, "N/A", false);
-                                echo $v;
-                            ?></td>
-                    <?php endforeach; ?>
-
-
-                    <td>
-                        <a href="edit_item.php?id=<?php se($record, "id"); ?>">Edit</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    <?php endif; ?>
+    <div class="page_traverser">
+    <?php
+    if($current_page >= 1){
+        echo("<a class='paginate_button' href = list_items.php?current_page=" . $current_page-1 . "&itemName=" . $itemName . "&itemCategory=" . $itemCategory . "&sortPrice=" . $sortPrice . "&outOfStock=" . $outOfStock . ">Previous</a>");
+    }
+    if(($current_page+1)*$PER_PAGE < $count_results["COUNT(*)"]){
+        echo("<a class='paginate_button' href = list_items.php?current_page=" . $current_page+1 . "&itemName=" . $itemName . "&itemCategory=" . $itemCategory. "&sortPrice=" . $sortPrice . "&outOfStock=" . $outOfStock . ">Next</a>");
+    }
+    echo("</div>");
+    $flopper=0;
+    echo("<div class='row'>");
+    foreach ($results as $index => $record){
+        $flopper++;
+        $card = <<<GODAN
+        <div class='info_card'>
+        <h2>{$record["name"]}</h2><br><br>
+        <h3>Type: {$record["category"]}</h3><br>
+        <h3>Cost: {$record["cost"]}</h3><br>
+        <a href='item_info.php?id={$record["id"]}'>Item info</a><br>
+        <a href='add_to_cart.php?id={$record["id"]}'>Add to cart</a><br>
+        GODAN;
+        if(has_role("Admin")){
+            $card .= "<a href='admin/edit_item.php?id={$record["id"]}'>Edit</a>";
+        }
+        $card .= "</div>";
+        if($flopper == 2){
+            $card .= "</div><div class='row'>";
+            $flopper = 0;
+        }
+        echo($card);
+    }
+    ?>
 </div>
 <?php
-require_once(__DIR__ . "/../../../partials/flash.php");
+require_once(__DIR__ . "../../../partials/flash.php");
 ?>
