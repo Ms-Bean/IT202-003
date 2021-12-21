90ce613cb094f5d15402594ff71d8ce3468ba1be
Spencer Clarke <sdc2@njit.edu>
2021-12-21 11:29:16 -0500

    flash

--- public_html/Project/item_info.php
+++ public_html/Project/item_info.php
@@ -133,8 +133,7 @@ if(is_logged_in()){
 //Get ratings
 $sqlstr = "SELECT rating, comment, user_id FROM Ratings WHERE product_id =:id LIMIT " . $PER_PAGE*$current_page . "," . $PER_PAGE;
 $count_str = "SELECT COUNT(*) FROM " . explode('LIMIT', explode('FROM', $sqlstr)[1])[0]; //Circumcise the sql string in order to obtain count
-echo($sqlstr);
-echo($count_str);
+flash($sqlstr);
 $stmt = $db->prepare($sqlstr);
 try {
     $stmt->execute([":id" => $id]);
